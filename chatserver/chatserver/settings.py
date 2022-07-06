c2854c9a384bf12d9f0622a90b44fd580cbac79c
fshangala <fshangala@gmail.com>
2022-07-06 20:46:58 +0200

    installed database

--- chatserver/chatserver/settings.py
+++ chatserver/chatserver/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'chatserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chatserver',
+        'USER': 'root',
+        'PASSWORD': 'shangala',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
